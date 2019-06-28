5b1597e490585f2e8e3130a19f460c00708512a1
Anamariabellop <am.bello@uniandes.edu.co>
2019-06-28 16:29:39 +0000

    en carpeta

---
+++ S4C4/holamundo.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+using namespace std;
+
+//programa 1: Haga un programa que Imprima "hola mundo". llamelo holamundo.cpp
+
+int main()
+	{
+	cout<<"Hello world"<< endl;
+    return 0;
+	}
